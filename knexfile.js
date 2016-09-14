4c4566b723071ba7b41b094e70e97d7333a56721
Tyler Chase <tyler.chase.usa@gmail.com>
2016-09-14 10:00:56 -0600

    first migration

--- knexfile.js
+++ knexfile.js
@@ -3,28 +3,11 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'postgres',
     connection: {
-      filename: './dev.sqlite3'
+      database: 'q2-tutorial-take-two'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'postgresql',
     connection: {
---
+++ migrations/20160914095947_todos.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('todos', function(table){
+    table.increments();
+    table.string('name');
+    table.string('description');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('todos')
+};
